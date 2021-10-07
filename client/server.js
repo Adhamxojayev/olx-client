52518e2ad0bd7b38e94f49d236a22d2b0fa1f90c
Ahror Adhamxojayev <axroradhamxojayev@gmail.com>
2021-10-07 13:53:39 +0500

    public static

--- client/server.js
+++ client/server.js
@@ -8,8 +8,6 @@ app.use( express.static(path.join(process.cwd(), 'client', 'public')) )
 
 app.get('/', (req, res) => res.sendFile(path.join(process.cwd(), 'client', 'views', 'index.html')))
 app.get('/reklama', (req, res) => res.sendFile(path.join(process.cwd(), 'client', 'views', 'moreinfo.html')))
-app.get('/reklama/css/styles.css', (req,res) => res.sendFile(path.join(process.cwd(),'client', 'public', 'css', 'styles.css')))
-app.get('/public/js/info.js', (req,res) => res.sendFile(path.join(process.cwd(),'client', 'public', 'js', 'info.js')))
 
 
 

cdd65cf1b2ef6a96e4ac3f396ee583a3713dc02b
Ahror Adhamxojayev <axroradhamxojayev@gmail.com>
2021-10-09 13:06:36 +0500

    poster page added

--- client/server.js
+++ client/server.js
@@ -8,7 +8,7 @@ app.use( express.static(path.join(process.cwd(), 'client', 'public')) )
 
 app.get('/', (req, res) => res.sendFile(path.join(process.cwd(), 'client', 'views', 'index.html')))
 app.get('/reklama', (req, res) => res.sendFile(path.join(process.cwd(), 'client', 'views', 'moreinfo.html')))
-
+app.get('/poster', (req,res) => res.sendFile(path.join(process.cwd(), 'client', 'views', 'poster.html')))
 
 
 app.listen(PORT, () => console.log(`server http://localhost:4000`))

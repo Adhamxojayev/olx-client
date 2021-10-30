300946bd0e5c2b4326eb88c4a22a534d7921d9f7
Ahror Adhamxojayev <axroradhamxojayev@gmail.com>
2021-10-30 17:08:19 +0500

    login register page

--- client/server.js
+++ client/server.js
@@ -1,14 +1,24 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import path from 'path'
 const PORT = process.env.PORT ?? 4000
 
 const app = express()
 
+app.use(cookieParser())
+
+
 app.use( express.static(path.join(process.cwd(), 'client', 'public')) )
 
 app.get('/', (req, res) => res.sendFile(path.join(process.cwd(), 'client', 'views', 'index.html')))
 app.get('/reklama', (req, res) => res.sendFile(path.join(process.cwd(), 'client', 'views', 'moreinfo.html')))
-app.get('/poster', (req,res) => res.sendFile(path.join(process.cwd(), 'client', 'views', 'poster.html')))
+app.get('/poster', (req,res) => {
+     if(req.cookies.userId){
+         res.sendFile(path.join(process.cwd(), 'client', 'views', 'poster.html'))
+     }else{
+         res.redirect('/register')
+     }
+})
 app.get('/register', (req,res) => res.sendFile(path.join(process.cwd(), 'client', 'views', 'register.html')))
 
 
